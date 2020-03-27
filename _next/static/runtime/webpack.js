fc46ff465acd7bfa1cf5265c3941efab06448c67
Jérôme Desboeufs <jerome.desboeufs@gmail.com>
2020-03-27 21:12:43 +0100

    Updates

--- _next/static/runtime/webpack.js
+++ _next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "22361e1b3ce27c3eefe7";
+/******/ 	var hotCurrentHash = "a168421100b43db25098";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
